402580e0bf4e4393237452ebf2282c41d71e90f0
aminulhaque98 <aminulhaque419@gmail.com>
2022-08-20 12:54:04 +0530

    first commit

---
+++ js/practices.js
@@ -0,0 +1,15 @@
+console.log('i love js')
+
+const getElement = document.getElementById('change-title');
+
+const getElementValueString = getElement.innerText;
+// console.log(getElementValueString);
+
+document.getElementById('btn-Buy').addEventListener('click', function () {
+
+    const getElement = document.getElementById('change-title');
+
+    const getElementValueString = getElement.innerText;
+    getElement.innerText = 'I AM PENGUIN'
+
+})
